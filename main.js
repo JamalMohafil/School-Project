1cbd58dca7b42f9d978c6686bd0978226167c8c9
Jamal Mohafil <109922894+JamalMohafil@users.noreply.github.com>
2022-11-29 16:22:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var search = document.getElementById("search");
 var clic = document.getElementById("click");
 var close =document.getElementById("close");
-
+var nav = document.getElementById('navv');
 clic.onclick = ()=>{
 search.style.display = "block";
 }
@@ -10,3 +10,10 @@ close.addEventListener('click',()=>{
     search.style.display = "none";
 
 })
+
+var hamburger = document.querySelector('.hamburger');
+
+hamburger.onclick = function (){
+     var navBar = document.querySelector('.nav-bar');
+navBar.classList.toggle("activee")
+    }
